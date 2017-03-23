9de2fd30350fa05d9235258c876b804500f384a0
Steve Garton <steve.garton@oraclefinance.co.uk>
2017-03-23 16:07:54 +0000

    Vagrant Config to test

---
+++ vagrant_files/bootstrap.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -q -y --force-yes screen htop vim curl wget
+apt-get install -q -y --force-yes python3 python git python-pip pkg-config
+apt-get install -q -y --force-yes python-dev htop supervisor build-essential
+apt-get install -q -y --force-yes python3-dev freetds-dev python-imaging libjpeg8 libjpeg62-dev libfreetype6
+apt-get install -q -y --force-yes libfreetype6-dev python-pip libxml2-dev libxmlsec1-dev libxslt1-dev zlib1g-dev
+apt-get install -q -y --force-yes redis-server redis-tools npm
+
+pip install virtualenvwrapper
+
+mkdir /var/log/xwing
+chown -R vagrant:vagrant /var/log/xwing
+
+# Copy Redis Config
+cp /vagrant/vagrant_files/redis /etc/redis/redis.conf
+service redis restart
+
+# Copy Supervisor Files
+cp /vagrant/vagrant_files/daphne.conf /etc/supervisor/conf.d/daphne.conf
+cp /vagrant/vagrant_files/workers.conf /etc/supervisor/conf.d/workers.conf
+
+# Update Supervisor & Start
+# Make sure Supervisor comes up after a reboot.
+systemctl enable supervisor
+
+# Bring Supervisor up right now.
+systemctl start supervisor
+supervisorctl reread
+supervisorctl update
---
+++ vagrant_files/bootstrap_user.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd /home/vagrant
+cp /vagrant/vagrant_files/.bashrc /home/vagrant/.bashrc
+cp /vagrant/vagrant_files/.inputrc /home/vagrant/.inputrc
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv xwing -p $(which python3)
+workon xwing
+cd /vagrant
+pip install -r requirements.txt
+echo "xwing virtualenv created, make sure you install requirements"
+byobu-enable
