28d9c50479d8fed73098ab30a4a4220293c7afdd
Steve Garton <steve.garton@oraclefinance.co.uk>
2017-03-23 23:11:16 +0000

    Remove unneeded packages

--- vagrant_files/bootstrap.sh
+++ vagrant_files/bootstrap.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -q -y --force-yes screen htop vim curl wget
-apt-get install -q -y --force-yes python3 python git python-pip pkg-config
-apt-get install -q -y --force-yes python-dev htop supervisor build-essential
-apt-get install -q -y --force-yes python3-dev freetds-dev python-imaging libjpeg8 libjpeg62-dev libfreetype6
-apt-get install -q -y --force-yes libfreetype6-dev python-pip libxml2-dev libxmlsec1-dev libxslt1-dev zlib1g-dev
-apt-get install -q -y --force-yes redis-server redis-tools npm
+apt-get install -q -y --force-yes python3 python git python-pip pkg-config redis-server redis-tools npm
+apt-get install -q -y --force-yes python-dev supervisor build-essential python3-dev python-pip
 #apt-get upgrade -y
 ln -s /usr/bin/nodejs /usr/bin/node
 pip install virtualenvwrapper
