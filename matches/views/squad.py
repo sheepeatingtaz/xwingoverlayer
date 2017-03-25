935c5615b0519f540998fbdf53294479f21e4c34
Steve Garton <steve.garton@oraclefinance.co.uk>
2017-03-25 09:00:21 +0000

    Control Interface improvements

--- matches/views/squad.py
+++ matches/views/squad.py
@@ -20,6 +20,10 @@ class ImportView(FormView):
         xws_file = form.cleaned_data['xws_file']
         reader = codecs.getreader("utf-8")
         data = json.load(reader(xws_file))
-        squad = Squad.objects.get(pk=import_squad(data, form.cleaned_data['player_name']))
-        messages.success(self.request, "{} successfully imported".format(squad.list_name))
+        squad_id = import_squad(data, form.cleaned_data['player_name'])
+        if squad_id:
+            squad = Squad.objects.get(pk=squad_id)
+            messages.success(self.request, "{} successfully imported".format(squad.list_name))
+        else:
+            messages.success(self.request, "Failed to import XWS file")
         return super().form_valid(form)
