0ec135ebc3cad3a716d53fe4bb1d1eba2df75621
Steve Garton <steve.garton@oraclefinance.co.uk>
2017-03-29 13:48:01 +0100

    First attempt at heroku Config

--- xwingoverlayer/settings.py
+++ xwingoverlayer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,10 +87,11 @@ WSGI_APPLICATION = 'xwingoverlayer.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'xwingoverlayer.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///xwingoverlayer.sqlite3')
+    #     {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'xwingoverlayer.sqlite3'),
+    # }
 }
 
 # Password validation
